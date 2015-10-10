5dad651804a9f55b1caa33d862253a6240e36a7c
BradRothenberg <bradrothenberg@gmail.com>
2015-10-09 20:51:02 -0400

    topop

---
+++ src/TopOpApp.cpp
@@ -0,0 +1,34 @@
+#include "TopOpt.h"
+#include <stdio.h>
+#include <memory>
+
+void PressEnterToContinue()
+{
+	int c;
+	printf("Press ENTER to continue... ");
+	fflush(stdout);
+	do c = getchar(); while ((c != '\n') && (c != EOF));
+}
+
+int main() {
+	const auto eX = 120;
+	const auto eY = 80;
+	const auto volFrac = .6;
+	const auto penal = 3.0;
+	const auto rmin = 3.0;
+	const auto maxChange = .01;
+
+	
+	
+auto topOpt = std::make_unique<TopOpt>(eX,eY,volFrac,penal,rmin,maxChange);
+
+double compliance, volume, change;
+bool go = true;
+
+while (go) {
+	go = topOpt->step(compliance, volume, change, true);
+}
+PressEnterToContinue();
+return 0;
+}
+
---
+++ src/TopOpt.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+#include <set>
+#include "Matrix.h"
+#include "MatrixBand.h"
+
+#ifndef tfloat
+#define tfloat double
+#endif
+
+using std::vector;
+
+class TopOpt {
+public:
+    // nelx, nely = number of elements in x and y direction 
+    // volfract = volume fraction
+    // penal = penalization value
+    // rmin = filter radius
+    TopOpt(int nelx, int nely, 
+		tfloat volfrac, tfloat penal, 
+		tfloat rmin, tfloat maxChange = 0.01);
+	~TopOpt();
+
+	bool step(tfloat& compliance, tfloat& volume, tfloat& change, bool verbose = false);
+	const Matrix& getX()const{ return *x; }
+	void setPassiveRadius(tfloat r) { mPassiveRadius = r; }
+private:
+	int mIter;
+	tfloat mMaxChange;
+	tfloat mChange;
+	Matrix xold;
+	tfloat mCurCompliance;
+	tfloat mCurVolume;
+	tfloat mPassiveRadius;
+private:
+    void FEAnalysis();
+    void initKE();
+    void meshIndependencyFilter(); // Also known as check
+    void optimalityCriteriaBasedOptimization();  // Also known as OC
+    vector<int> setDiff(vector<int> &set1, vector<int> &set2);
+    void initFreeDofs();
+    void defineLoads();
+    void calculatePassive();
+    void exportResultMatlab(Matrix *x, int iteration);
+	void exportResultTxt(Matrix *x, int iteration);
+private:
+
+    tfloat objectiveFunctionAndSensitivityAnalyses();
+    tfloat interpolate(int x, int y);
+    tfloat interpolateDiff(int x, int y);
+    int nelx; // number of element along x-axis
+    int nely; // number of element along y-axis
+    tfloat volfrac; // volume fraction
+    tfloat penal;
+    tfloat rmin;
+    std::shared_ptr<Matrix> x;
+    std::shared_ptr<Matrix> xNew;
+
+	std::shared_ptr<Matrix> dc;
+	std::shared_ptr<Matrix> dcNew;
+
+    std::vector<tfloat> U; // Displacement vectors
+	std::vector<tfloat> F; // Displacement vectors
+
+    Matrix KE; // stiffness matrix
+
+    MatrixBand K;
+    vector<int> passiveNoMaterial;
+    vector<int> fixeddofs;
+    vector<int> freedofs;
+};
